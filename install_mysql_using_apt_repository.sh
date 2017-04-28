b044bd2f69e5e51d670b8e8e3de00f726864d3a2
ELOUIZ BADR <elouiz.badr@gmail.com>
2017-04-28 11:33:44 +0100

    Cluster files!

--- install_mysql_using_apt_repository.sh
+++ install_mysql_using_apt_repository.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Download MySQL's APT Repository for Debian based OSes
-sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.5-1_all.deb
+sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.4-1_all.deb
 
 # Install MySQL's APT Repository
-sudo dpkg -i mysql-apt-config_0.8.5-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.4-1_all.deb
 
 # Update the APT Packages
 sudo apt-get update
 
 # Install MySQL Server (Latest GA version) and MySQL Shell for Administration purposes
-sudo apt-get install -y mysql-server mysql-shell
+#sudo apt-get install -y mysql-server mysql-shell

193fee63feb5359f08d04c1c0cf452ec4cae1cac
ELOUIZ BADR <elouiz.badr@gmail.com>
2017-05-06 15:46:56 +0100

    Now support Ubuntu !

--- install_docker_debian.sh
+++ install_docker_debian.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
+OSNAME=${OSNAME:-debian}
 
 # Install dependencies
 echo "Installing dependencies ... "
-sudo apt-get install -y \
- apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+ apt-transport-https ca-certificates curl gnupg2 \
+ software-properties-common lsb-release
 
 # Add Docker's official GPG key
 echo "Adding Docker's official GPG key ... "
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/$OSNAME/gpg | sudo apt-key add -
 
 # Add Docker's APT repository
 sudo add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+"deb [arch=amd64] https://download.docker.com/linux/$OSNAME $(lsb_release -cs) stable"
 echo "Adding Docker repository to APT ... done!"
 
 # Install Docker-CE
@@ -24,6 +26,10 @@ sudo apt-get install -y docker-ce
 # Verify Docker-CE Installation
 echo "Verifying Docker-CE installation ... "
 sudo docker run hello-world
+
+# Enable Docker to start with OS
+echo "Enabling Docker with SystemD ... "
+sudo systemctl enable docker
 
 # To use Docker-CE without "sudo"
 sudo usermod -aG docker $USER

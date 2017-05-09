3aadb395bb478a9250708da7c772b1ec091e7a59
ELOUIZ BADR <elouiz.badr@gmail.com>
2017-05-09 15:02:49 +0100

    Almost there, Percona XtraDB Cluster with HAProxy!

--- haproxy/run_haproxy.sh
+++ haproxy/run_haproxy.sh
@@ -2,3 +2,4 @@
 set -e
 
 docker run -d -p 3306:3306 -p 8080:8080 --name my-running-haproxy my-haproxy
+#docker exec my-running-haproxy /bin/bash -c "apt-get update && apt-get install -y --no-install-recommends telnet nano mysql-client"

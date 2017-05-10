9e1c8debbd5bf9d6bb0361f8b427e45418fa416f
ELOUIZ BADR <elouiz.badr@gmail.com>
2017-05-10 16:33:20 +0100

    Fix "xinetd" service start bug !

--- haproxy/configure_haproxy.sh
+++ haproxy/configure_haproxy.sh
@@ -2,33 +2,16 @@
 set -e
 source ~/cluster/percona_cluster.cfg
 
-# Configuration for HAProxy
-echo -n "Configuring Cluster nodes for HAProxy " ;
-for i in `seq 1 30`; do
-	sleep 1
-	echo -n ". "
-done
-echo ":"
-for i in `seq 1 $((CLUSTER_NODES_SIZE))`; do
-	echo "Node #$i :"
-	echo "	- configuring MySQL Check script!"
-	docker exec $CLUSTER_NODES_NAME$i /bin/bash -c "echo 'mysqlchk	9200/tcp	# mysqlchk' >> /etc/services "
-	echo "	- configuring Cluster Check account!"
-	docker exec $CLUSTER_NODES_NAME$i /bin/bash -c "mysql -u root -proot -e \"grant process on *.* to 'clustercheckuser'@'%' identified by 'clustercheckpassword!';flush privileges;\" "
-	echo "	- configuring MySQL Check script!"
-	docker exec $CLUSTER_NODES_NAME$i /bin/bash -c "apt-get update && apt-get install -y --no-install-recommends telnet nano xinetd" > /dev/null
-	docker exec $CLUSTER_NODES_NAME$i /bin/bash -c "service xinetd start" > /dev/null
-done
-
-echo "Configuring HAProxy :"
 # First, build HAProxy Docker image
+echo "Configuring HAProxy . . . "
 echo "	- Building HAProxy Docker image!"
 docker build -t "haproxy:latest" ~/cluster/haproxy/
 
 # Second, verify configuration file for HAProxy
 echo "	- Verifying HAProxy configuration file!"
-docker run -it --rm --name haproxy-syntax-check haproxy haproxy \
-	-c -f /usr/local/etc/haproxy/haproxy.cfg
+docker run -it --rm \
+	--name haproxy-syntax-check \
+	haproxy haproxy	-c -f /usr/local/etc/haproxy/haproxy.cfg
 
 # Last, run HAProxy Docker container
 echo "	- Running HAProxy Docker container!"

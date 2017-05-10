aa5b89722e2eae2f96c6d581383c5f5491d791f7
ELOUIZ BADR <elouiz.badr@gmail.com>
2017-05-10 17:23:21 +0100

    Performance enhancements !

--- create_percona_cluster.sh
+++ create_percona_cluster.sh
@@ -10,6 +10,7 @@ IFS=. read octet1 octet2 octet3 octet4 <<< "$CLUSTER_NETWORK"
 # Launch the first node which is responsible for initializing the Cluster
 echo "Initiatlizing Percona cluster ... "
 number=1
+
 docker run -d \
 	--name "$CLUSTER_NODES_NAME$number" \
 	--hostname "$CLUSTER_NODES_HOSTNAME$number" \
@@ -17,6 +18,9 @@ docker run -d \
 	-e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
 	-e XTRABACKUP_PASSWORD=$XTRABACKUP_PASSWORD \
 	albodor/percona-cluster-haproxy
+rm -f haproxy/haproxy.cfg
+cp haproxy/haproxy.cfg.sample haproxy/haproxy.cfg
+echo "	server $CLUSTER_NODES_NAME$number 172.17.0.2:3306 check port 9200 inter 12000 rise 3 fall 3" >> haproxy/haproxy.cfg
 echo "done!"
 echo -n "Waiting for first Cluster node to prepare "
 for i in `seq 1 10`; do
@@ -36,6 +40,7 @@ for j in `seq 2 $((CLUSTER_NODES_SIZE))`; do
 		-e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
 		-e XTRABACKUP_PASSWORD=$XTRABACKUP_PASSWORD \
 	albodor/percona-cluster-haproxy
+	echo "	server $CLUSTER_NODES_NAME$j $octet1.$octet2.$octet3.$((j+1)):3306 check port 9200 inter 12000 rise 3 fall 3" >> haproxy/haproxy.cfg
         sleep 2
 	echo "- Adding node $j to the Cluster : done !"
 done

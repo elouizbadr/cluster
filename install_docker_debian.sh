29234361c02a29c9b4794d62b9f91627db4d0b9d
ELOUIZ BADR <elouiz.badr@gmail.com>
2017-06-18 16:12:42 +0000

    Adding Usage Comments and Bugs Fixed!

--- install_docker_debian.sh
+++ install_docker_debian.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+###
+# Requirments: "sudo" package must be already installed and configured.
+# Recommended: Run this script with a non-priveleged user account.
+###
+
 set -e
 OSNAME=${OSNAME:-debian}
 DockerComposeVersion=${DockerComposeVersion:-1.13.0}
-
 
 # Install dependencies
 echo "Installing dependencies ... "
@@ -26,17 +31,21 @@ echo "Installing latest Docker-CE ... "
 sudo apt-get install -y docker-ce
 echo "Installing Docker Compose v$DockerComposeVersion ... "
 curl -L https://github.com/docker/compose/releases/download/$DockerComposeVersion/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Verify Docker-CE Installation
 echo "Verifying Docker-CE installation ... "
 sudo docker run hello-world
 
-# Enable Docker to start with OS
+# Enable Docker-CE to start with OS
 echo "Enabling Docker with SystemD ... "
 sudo systemctl enable docker
 
 # To use Docker-CE without "sudo"
-sudo usermod -aG docker $USER
-echo "Running postinstallation configuration ... done! "
+if [ "$EUID" -ne 0 ]
+  then sudo usermod -aG docker $USER
+  echo "Running postinstallation configuration ... done! "
+  echo "Please log out and log back to be able to run docker commands without 'sudo'."
+fi
+
 echo "Docker-CE installed Successfully!"
-echo "Please log out and log back to be able to run docker commands without 'sudo'."
